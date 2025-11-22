6737839c09bc103ff432b4ee2b476c35d485fbee
NguyenManhTien1505 <dungdendui0@gmail.com>
2025-11-22 15:55:57 +0700

    Bai kiem tra lan 2

---
+++ NguyenManhTien-LTW/NguyenManhTIen_231230923/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenManhTIen_231230923.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
